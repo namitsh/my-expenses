214b1037d87b5e9f7e8532db0119d15aa7b70b93
Namit Sharda <namit.sharda63@gmail.com>
2022-01-09 22:57:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// basic express config.
+app.use(morgan('dev'))
+app.disable('x-powered-by');
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+// routers;
+
+
+// Handle 404
+app.use(function(req, res, next) {
+    res.status(404);
+    res.json({"message": "Page not found"})
+    // TODO: WORK ON ERROR HANDLING 
+    next();
+});
+  
+// Handle 500
+app.use(function(error, req, res, next) {
+    // TODO: create a errorHandler middleware.
+    res.send('500: Internal Server Error', 500);
+});
+
+module.exports = app;
---
+++ config/mongodb.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+function MongoClient() {
+    this.options = {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    };
+    this.connectionUri = process.env.MONGO_DB_URI || 'mongodbs://localhost:27017/my-expenses';
+}
+
+MongoClient.prototype.initialize = function(){
+    mongoose
+        .connect(this.connectionUri, this.options)
+        .then(()=>{
+            console.log("DATABASE CONNECTED SUCCESSFULLY...");
+        })
+        .catch(err=>{
+            console.error(err);
+            process.exit(1);
+        })
+}
+
+module.exports = new MongoClient();
